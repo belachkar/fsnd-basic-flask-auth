c42547ba0d6f37c88e8235b186fd106d42ba7f10
belachkar <belachkar_ali@hotmail.com>
2020-02-01 09:41:25 +0100

    Add check permissions function

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 AUTH0_DOMAIN = "auth-secure.auth0.com"
 ALGORITHMS = ["RS256"]
-API_AUDIENCE = "coffee"
+API_AUDIENCE = "image"
 
 
 class AuthError(Exception):
@@ -58,6 +58,7 @@ def verify_decode_jwt(token):
     jsonurl = urlopen(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
+    # print(unverified_header)
     rsa_key = {}
     if "kid" not in unverified_header:
         raise AuthError({"code": "invalid_header", "description": "Authorization malformed."}, 401)
@@ -104,23 +105,46 @@ def verify_decode_jwt(token):
     )
 
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except Exception:
-            abort(401)
-        return f(payload, *args, **kwargs)
+def check_permissions(permission, payload):
+    if "permissions" not in payload:
+        raise AuthError(
+            {"code": "invalid_claims", "description": "Permissions not included in JWT."}, 400
+        )
 
-    return wrapper
+    print(permission, payload)
+    if permission not in payload["permissions"]:
+        raise AuthError({"code": "unauthorized", "description": "Permission not found."}, 403)
+    return True
+
+
+def requires_auth(permissions=""):
+    def requires_auth_decorator(f):
+        # @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except Exception:
+                abort(401)
+            check_permissions(permissions, payload)
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+
+    return requires_auth_decorator
 
 
 @app.route("/headers")
 @requires_auth
 def headers(payload):
     print(payload)
+    return "Access Granted"
+
+
+@app.route("/images")
+@requires_auth("get:images")
+def images(payload):
+    print(jwt)
     return "Access Granted"
 
 
